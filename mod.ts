5a7e73f3811e60cc50670dbcf2c134c08416e1c1
Foxi <noxifoxi@gmail.com>
2021-09-12 21:15:00 +0200

    wrapping everything into a single class

--- mod.ts
+++ mod.ts
@@ -1,11 +1,11 @@
 const { build, run, readAll } = Deno;
 
-export interface Process {
+export interface Proc {
 	command: string; // Command to run this process
 	ppid: number; // The parent process ID of the process
 	pid: number; // Process ID
 	stat: string; // Process status
-	children?: Process[];
+	children?: Proc[];
 }
 
 export interface KillOptions {
@@ -14,158 +14,161 @@ export interface KillOptions {
 	tree?: boolean;
 }
 
-/**
- * Get the single process information.
- * Requires `--allow-run` flag
- * @param pid
- */
-export async function get(pidOrName: number | string): Promise<Process | void> {
-	if (typeof pidOrName === 'number')
-		return (await getAll()).find((v) => v.pid === pidOrName);
-	return (await getAll()).find((v) => v.command === pidOrName);
-}
-
-/**
- * Get process list
- * Requires `--allow-run` flag
- */
-export async function getAll(): Promise<Process[]> {
-	const commands = build.os == "windows"
-		? ["wmic.exe", "PROCESS", "GET", "Name,ProcessId,ParentProcessId,Status"]
-		: ["ps", "-A", "-o", "comm,ppid,pid,stat"];
-
-	const ps = run({
-		cmd: commands,
-		stdout: "piped",
-	});
-
-	const output = new TextDecoder().decode(await readAll(ps.stdout!));
-
-	const { success, code } = await ps.status();
-
-	ps.stdout?.close();
-
-	ps.close();
-
-	if (!success || code !== 0) {
-		throw new Error("Fail to get process.");
+export class Process {
+	/**
+	 * Get the single process information.
+	 * Requires `--allow-run` flag
+	 * @param pidOrName
+	 */
+	static async get(pidOrName: number | string): Promise<Proc | void> {
+		if (typeof pidOrName === 'number')
+			return (await Process.getAll()).find((v) => v.pid === pidOrName);
+		return (await Process.getAll()).find((v) => v.command === pidOrName);
 	}
 
-	const lines = output.split("\n").filter((v: string): string => v.trim());
-	lines.shift();
+	/**
+	 * Get process list
+	 * Requires `--allow-run` flag
+	 */
+	static async getAll(): Promise<Proc[]> {
+		const commands = build.os == "windows"
+			? ["wmic.exe", "PROCESS", "GET", "Name,ProcessId,ParentProcessId,Status"]
+			: ["ps", "-A", "-o", "comm,ppid,pid,stat"];
 
-	const processList: Process[] = lines.map(
-		(line: string): Process => {
-			const columns = line.trim().split(/\s+/);
-			return {
-				command: columns[0],
-				ppid: +columns[1],
-				pid: +columns[2],
-				stat: columns[3],
-			};
-		},
-	);
+		const ps = run({
+			cmd: commands,
+			stdout: "piped",
+		});
 
-	return processList;
-}
+		const output = new TextDecoder().decode(await readAll(ps.stdout!));
 
-/**
- * Get process tree
- * Requires `--allow-run` flag
- */
-export async function getTree(): Promise<Process[]> {
-	const items = await getAll();
-	const nest = (items: Process[], pid: number = 0): Process[] => {
-		return items
-			.filter((item) => item.ppid === pid)
-			.map((item) => {
-				const children = nest(items, item.pid);
-				if (!children.length) {
-					return item;
-				} else {
-					return { ...item, children };
-				}
-			}) as Process[];
-	};
+		const { success, code } = await ps.status();
 
-	return nest(items);
-}
+		ps.stdout?.close();
 
-function getKillCommand(
-	pidOrName: number | string,
-	options: KillOptions = {},
-): string[] {
-	const killByName = typeof pidOrName === "string";
-	if (build.os === "windows") {
-		const commands = ["taskkill"];
+		ps.close();
 
-		if (options.force) {
-			commands.push("/f");
+		if (!success || code !== 0) {
+			throw new Error("Fail to get process.");
 		}
 
-		if (options.tree) {
-			commands.push("/t");
-		}
+		const lines = output.split("\n").filter((v: string): string => v.trim());
+		lines.shift();
 
-		commands.push(killByName ? "/im" : "/pid", pidOrName + "");
+		const processList: Proc[] = lines.map(
+			(line: string): Proc => {
+				const columns = line.trim().split(/\s+/);
+				return {
+					command: columns[0],
+					ppid: +columns[1],
+					pid: +columns[2],
+					stat: columns[3],
+				};
+			},
+		);
 
-		return commands;
-	} else if (build.os === "linux") {
-		const commands = [killByName ? "killall" : "kill"];
-
-		if (options.force) {
-			commands.push("-9");
-		}
-
-		if (killByName && options.ignoreCase) {
-			commands.push("-I");
-		}
-
-		commands.push(pidOrName + "");
-
-		return commands;
-	} else {
-		const commands = [killByName ? "pkill" : "kill"];
-
-		if (options.force) {
-			commands.push("-9");
-		}
-
-		if (killByName && options.ignoreCase) {
-			commands.push("-i");
-		}
-
-		commands.push(pidOrName + "");
-
-		return commands;
+		return processList;
 	}
-}
 
-/**
- * kill process
- * Requires `--allow-run` flag
- * @param pidOrName pid or process name
- * @param options
- */
-export async function kill(
-	pidOrName: number | string,
-	options: KillOptions = {},
-): Promise<void> {
-	const commands = getKillCommand(pidOrName, options);
+	/**
+	 * Get process tree
+	 * Requires `--allow-run` flag
+	 */
+	static async getTree(): Promise<Proc[]> {
+		const items = await Process.getAll();
+		const nest = (items: Proc[], pid = 0): Proc[] => {
+			return items
+				.filter((item) => item.ppid === pid)
+				.map((item) => {
+					const children = nest(items, item.pid);
+					if (!children.length) {
+						return item;
+					} else {
+						return { ...item, children };
+					}
+				}) as Proc[];
+		};
 
-	const ps = run({
-		cmd: commands,
-		stderr: "piped",
-	});
-
-	const { success, code } = await ps.status();
-
-	ps.stderr?.close();
-
-	ps.close();
-
-	if (!success || code !== 0) {
-		const msg = new TextDecoder().decode(await readAll(ps.stderr!));
-		throw new Error(msg || "exit with code: " + code);
+		return nest(items);
 	}
+
+	static getKillCommand(
+		pidOrName: number | string,
+		options: KillOptions = {},
+	): string[] {
+		const killByName = typeof pidOrName === "string";
+		if (build.os === "windows") {
+			const commands = ["taskkill"];
+	
+			if (options.force) {
+				commands.push("/f");
+			}
+	
+			if (options.tree) {
+				commands.push("/t");
+			}
+	
+			commands.push(killByName ? "/im" : "/pid", pidOrName + "");
+	
+			return commands;
+		} else if (build.os === "linux") {
+			const commands = [killByName ? "killall" : "kill"];
+	
+			if (options.force) {
+				commands.push("-9");
+			}
+	
+			if (killByName && options.ignoreCase) {
+				commands.push("-I");
+			}
+	
+			commands.push(pidOrName + "");
+	
+			return commands;
+		} else {
+			const commands = [killByName ? "pkill" : "kill"];
+	
+			if (options.force) {
+				commands.push("-9");
+			}
+	
+			if (killByName && options.ignoreCase) {
+				commands.push("-i");
+			}
+	
+			commands.push(pidOrName + "");
+	
+			return commands;
+		}
+	}
+
+	/**
+	 * kill process
+	 * Requires `--allow-run` flag
+	 * @param pidOrName pid or process name
+	 * @param options
+	 */
+	static async kill(
+		pidOrName: number | string,
+		options: KillOptions = {},
+	): Promise<void> {
+		const commands = Process.getKillCommand(pidOrName, options);
+
+		const ps = run({
+			cmd: commands,
+			stderr: "piped",
+		});
+
+		const { success, code } = await ps.status();
+
+		ps.stderr?.close();
+
+		ps.close();
+
+		if (!success || code !== 0) {
+			const msg = new TextDecoder().decode(await readAll(ps.stderr!));
+			throw new Error(msg || "exit with code: " + code);
+		}
+	}
+
 }
